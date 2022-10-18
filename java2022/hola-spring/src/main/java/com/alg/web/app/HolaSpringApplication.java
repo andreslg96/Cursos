32b10a58c8f46ef492633e82a3c12f19b7694fb4
Andrés López <andres.lg96@outlook.com>
2022-10-17 22:45:43 -0500

    s21: Spring Boot

---
+++ java2022/hola-spring/src/main/java/com/alg/web/app/HolaSpringApplication.java
@@ -0,0 +1,21 @@
+package com.alg.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Controller
+public class HolaSpringApplication {
+	
+	@GetMapping("/hola")
+	public String hola() {
+		return "index";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(HolaSpringApplication.class, args);
+	}
+
+}
