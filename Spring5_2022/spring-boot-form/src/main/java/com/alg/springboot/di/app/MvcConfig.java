16869f8ac442154869135384fa6137b924044d77
Andrés López <andres.lg96@outlook.com>
2022-12-06 19:25:48 -0600

    S05 sesiones 91-93

---
+++ Spring5_2022/spring-boot-form/src/main/java/com/alg/springboot/di/app/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.alg.springboot.di.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig  implements WebMvcConfigurer{
+	
+	@Autowired
+	@Qualifier("tiempoTranscurridoInterceptor")
+	private HandlerInterceptor timepoTranscurridoInterceptor; 
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(timepoTranscurridoInterceptor); //.addPathPatterns("/form/**") para ponerle una direcciónS
+	}
+	
+}
